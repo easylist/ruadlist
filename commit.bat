fa73c9501234c370806e509a6a86a717bce621c0
lain.halfbit <none@none>
2010-10-19 07:09:31 +0000

    Дополнил скрипты коммита - добавил вызов операции обновления подписки перед расчётом контрольных сумм. Это должно решить проблему с периодической необходимостью выполнять разрешение конфликта контрольной суммы если забыл обновить подписку перед коммитом.

--- commit.bat
+++ commit.bat
@@ -1,5 +1,14 @@
+@echo off
+@echo Updating subscribtions...
+tortoiseproc /command:update /path:"." /closeonend:3
+@echo Calculating checksums:
 checksum.pl advblock.txt
+@echo * advblock.txt
 checksum.pl antinuha.txt
+@echo * antinuha.txt
 checksum.pl bitblock.txt
+@echo * bitblock.txt
 checksum.pl cntblock.txt
+@echo * cntblock.txt
+@echo Commiting changes to server...
 tortoiseproc /command:commit /path:"."
