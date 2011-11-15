9925728106c269013f5342a1f0eb6d9ec32667d6
lain.halfbit <none@none>
2011-11-15 23:42:10 +0000

    Удалил ставший ненужным checklist.txt, закольцевал doa.pl на проверку доменов из списка «мёртвых» и настроил вывод pingen туда же (если понадобиться перегенерировать список в будущем). Смысл в том, что если домен хоть иногда определяется, то я не хочу его удалять из подписки. Смысла же перепроверять то, что и так уже несколько раз проверено и гарантированно работает просто нет. Так-же удалил случайно попавший мусор и пингующиеся айпишники.

--- resolver/doa.pl
+++ resolver/doa.pl
@@ -1,35 +1,58 @@
 #!/usr/bin/perl
 
-#use Net::Ping;
 use Net::DNS;
 
-open(INFILE, "<", "checklist.txt") or die("cannot open infile:  $!");
+open(INFILE, "<", "deadhosts.txt") or die("cannot open infile:  $!");
 
 my @ip_array = <INFILE>;
 
 close(INFILE);
 
-open(OUTFILE, ">", "deadhosts.txt") or die("unable to write output: $!");
+open(OUTFILE, ">", "deadhosts.tmp") or die("unable to write output: $!");
 
 chomp(@ip_array);
 
-#$p = Net::Ping->new("icmp", 3, 64);
 $p = Net::DNS::Resolver->new;
 
 foreach(@ip_array)
   {
    if($_ =~ /^.*$/)
-      { 
-	if(not $p->query($&))
+      {
+	$q = $p->query($&);
+	if(not $q)
           {
-            print ("D $&\n");
-            print OUTFILE ("$&\n");
+            print "D $& ", $p->errorstring, "\n";
+            print OUTFILE "$&\n";
           }
 	else
           {
-            print ("A $&\n");
+            print "A $& ";
+	    foreach my $rr ($q->answer)
+              {
+		next unless $rr->type eq "A";
+		print $rr->address, "\n";
+	      }
           }
       }	
   } 
 
 close(OUTFILE);
+
+unlink("deadhosts.txt");
+
+rename("deadhosts.tmp","deadhosts.txt");
+
+print "\n";
+print "NOERROR  A NOERROR indicates that the domain does exist", "\n";
+print "         according to the root name servers and that the", "\n";
+print "         authoritative name servers are answering queries", "\n";
+print "         correctly for that domain.", "\n\n";
+print "SERVFAIL SERVFAIL means that the domain does exist and the", "\n";
+print "         root name servers have information on this domain,", "\n";
+print "         but that the authoritative name servers are not", "\n";
+print "         answering queries for this domain.", "\n\n";
+print "NXDOMAIN NXDOMAIN can means that the root name servers are", "\n";
+print "         not providing any authoritative name servers for", "\n";
+print "         this domain. This can be because the domain does", "\n";
+print "         not exist or that the domain is on-hold.", "\n";
+
