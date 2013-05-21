7d409298f87d3593cc94a0abb3141980abb6f78e
lain.halfbit <none@none>
2013-05-21 20:26:19 +0300

    Похожие с EasyList правила. Переделал правила для http://город-кингисепп.рф/ Удалил правило для уроки-на-диване.рф (переехал и правило более не нужно).

--- .commit.bat
+++ .commit.bat
@@ -13,6 +13,8 @@ hash.pl bitblock.txt
 @echo * bitblock.txt
 hash.pl cntblock.txt
 @echo * cntblock.txt
+@echo Разница:
+hg diff -U0 --nodates
 @echo Внесение изменений на сервер...
 hg commit
 hg push
