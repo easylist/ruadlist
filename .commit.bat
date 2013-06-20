e82f40108af11952dcc7d7f5849d53c8fe1aab29
lain <none@none>
2013-06-20 19:29:01 +0300

    Считать хэши больше не нужно. :)

--- .commit.bat
+++ .commit.bat
@@ -4,15 +4,6 @@ hg pull
 hg update --check
 hg update
 hg merge
-@echo Расчёт контрольных сумм:
-hash.pl advblock.txt
-@echo * advblock.txt
-hash.pl antinuha.txt
-@echo * antinuha.txt
-hash.pl bitblock.txt
-@echo * bitblock.txt
-hash.pl cntblock.txt
-@echo * cntblock.txt
 @echo Разница:
 hg diff --nodates
 @echo Внесение изменений на сервер...
