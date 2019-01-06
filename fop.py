bd5adf19ccea8adbe4868637db2e82c1a0030854
lain <none@none>
2019-01-06 23:40:13 +0200

    M: Support for uBO's "badfilter" and "first-party" options.

--- fop.py
+++ fop.py
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>."""
 # FOP version number
-VERSION = 3.810
+VERSION = 3.811
 
 # Import the key modules
 import collections, filecmp, os, re, subprocess, sys
@@ -63,8 +63,8 @@ IGNORE = ("CC-BY-SA.txt", "easytest.txt", "GPL.txt", "MPL.txt", "antinuha.txt",
 # List all Adblock Plus options (excepting domain, which is handled separately), as of version 1.3.9
 KNOWNOPTIONS = ("collapse", "document", "elemhide", "font", "generichide", "genericblock",
                 "image", "match-case", "media", "object", "object-subrequest", "other",
-                "ping", "popup", "script", "stylesheet", "subdocument",
-                "third-party", "websocket", "xmlhttprequest")
+                "ping", "popup", "script", "stylesheet", "subdocument", "badfilter",
+                "first-party", "third-party", "websocket", "xmlhttprequest")
 
 # List the supported revision control system commands
 REPODEF = collections.namedtuple("repodef", "name, directory, locationoption, repodirectoryoption, checkchanges, difference, pull, checkupdate, update, merge, commit, push")
